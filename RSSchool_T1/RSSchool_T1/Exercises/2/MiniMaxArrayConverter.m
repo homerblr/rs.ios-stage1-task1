17af4fb6809c8c5cfcf502ee6047524f2d5cb378
Mikhail Kisly <salodci@gmail.com>
2020-03-16 20:32:21 +0300

    feat: resolve Task1

--- RSSchool_T1/RSSchool_T1/Exercises/2/MiniMaxArrayConverter.m
+++ RSSchool_T1/RSSchool_T1/Exercises/2/MiniMaxArrayConverter.m
@@ -2,9 +2,21 @@
 
 @implementation MiniMaxArrayConverter
 
-// Complete the convertFromArray function below.
+
 - (NSArray<NSNumber*>*)convertFromArray:(NSArray<NSNumber*>*)array {
-    return @[@0];
+  NSMutableArray *tmp = [[[NSMutableArray alloc] initWithArray:array] sortedArrayUsingComparator:^NSComparisonResult(id a, id b) {
+    return [a compare:b];
+  }];
+  NSNumber * sum = [array valueForKeyPath:@"@sum.self"];
+  
+  NSMutableArray *result = [[NSMutableArray alloc] init];
+  
+  [result addObject:[NSNumber numberWithInt:( [sum intValue] - [[tmp lastObject] intValue])]];
+  [result addObject:[NSNumber numberWithInt:( [sum intValue] - [[tmp objectAtIndex:0] intValue])]];
+  
+  return result;
+  
 }
+
 
 @end
--- RSSchool_T1/RSSchool_T1/Exercises/4/Parser.h
+++ RSSchool_T1/RSSchool_T1/Exercises/4/Parser.h
@@ -2,7 +2,10 @@
 
 @interface Parser : NSObject
 
+@property (nonatomic, retain) NSMutableArray *result;
+
 - (NSArray <NSString*>*)parseString: (NSString*)string;
+- (void)handleCase: (NSString *)string position:(int)i bracket:(NSString *)openBracket;
 
 @end
 
